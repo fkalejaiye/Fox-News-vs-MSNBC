f5545218ab609c5c9851ea6eb7dad5fdb08005c0
Femi Kalejaiye <femikalejaiye@Femis-MacBook-Pro.local>
2020-05-07 20:59:35 -0500

    Finished both scrapers

--- occ_dem_web_scraper.py
+++ occ_dem_web_scraper.py
@@ -5,28 +5,25 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-
-pages = np.arange(1,401)
-
+pages = np.arange(1,668)
 client = MongoClient()
-breitbart_db=client['breitbart']
-urls = breitbart_db['urls']
-articles = breitbart_db['articles']
+occupy_democrats_db=client['occupy_democrats']
+urls = occupy_democrats_db['urls']
+articles = occupy_democrats_db['articles']
 
 def get_urls(x):
     links=[]
-    count=0
-    first_half = "https://www.breitbart.com"
     for num in x:
-        pg = requests.get(f'https://www.breitbart.com/politics/page/{num}/')
+        pg = requests.get(f'https://occupydemocrats.com/category/politics/page/{num}/')
         if pg.status_code==200:
             soup = BeautifulSoup(pg.text,"lxml")
-            data = soup.findAll('div', {'class':"tC"})
+            data = soup.findAll('div', {'class':"post-title"})
         else:
+            print(pg.status_code)
             continue
             
         for i in data:
-            link=first_half+(i.find('a').get("href"))
+            link=(i.find('a').get("href"))
             if link not in links:
                 links.append(link)          
         print(f'Scraped {len(links)} urls.')
@@ -35,31 +32,29 @@ def get_urls(x):
         urls.insert_one({'link': link})
     print("Done!!")
 
-
 def get_articles(urls_of_articles):
     count=0
     for url in urls_of_articles:
         pg = requests.get(url)
         if pg.status_code==200:
             soup = BeautifulSoup(pg.text,"lxml")
-            title = soup.find('h1').text
-            author = soup.find('div', {"class": 'header_byline'}).find('a').text
-            date = soup.find('div', {"class": 'header_byline'}).find('time').text
-            shares = soup.find('span', {"class": 'acz5'}).text
-            
-            data = soup.findAll('div', {'class':"entry-content"})
+            title = soup.find('h1').text.strip()
+            author = soup.find('div', {"class": 'author-content'}).find('a').text
+            date = soup.find('div', {"class": 'thb-post-date'}).text.strip()
+            data = soup.findAll('div', {'class':"post-content entry-content"})
             content=[]
             for i in data:
                 for j in i.find_all('p'):
                     t = j.get_text()
                     content.append(t)
+                    
             content = " ".join(content)
             articles.insert_one({'title':title, 'author':author, 'date': date, 'content': content})
             count+=1
             print(f'Scraped {count} articles. ({title})')
             time.sleep(2)
+            
         else:
-            print('Failed to get page!')
             continue
 
 websites = [urls.find()[i]['link'] for i in range(5001)]
